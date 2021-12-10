b8423afa7c1711c34eecb6d382482a64f378f7ca
Steph Peets <stephanie.peets@gmail.com>
2021-12-09 20:21:06 -0500

    list columns and their datatypes

--- queries.sql
+++ queries.sql
@@ -1,5 +1,12 @@
 -- Part 1: Test it with SQL
 
+--    column, DATATYPE
+      -----------------
+--    `id`, INT
+--    `employer`, VARCHAR
+--    `name`, VARCHAR
+--     `skills`, VARCHAR
+
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
