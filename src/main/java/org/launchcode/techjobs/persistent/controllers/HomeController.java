75bb2e07a7d31575b5233ea64057f7fa516c3e97
Steph Peets <stephanie.peets@gmail.com>
2021-12-11 20:12:53 -0500

    add Skill.jobs field

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.persistent.controllers;
 import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +24,9 @@ public class HomeController {
 
     @Autowired
     EmployerRepository employerRepository;
+
+    @Autowired
+    SkillRepository skillRepository;
 
     @RequestMapping("")
     public String index(Model model) {
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -17,7 +17,7 @@ public class Employer extends AbstractEntity {
 
 	@OneToMany
 	@JoinColumn(name = "employer_id")
-	private List<Job> jobs= new ArrayList();
+	private List<Job> jobs = new ArrayList();
 
 	public Employer() {
 	}
